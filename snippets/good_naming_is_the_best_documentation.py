f11c0b64fb979d8ca04829469dcee3490aafe3ce
Emilio Carrión <emcaryp@gmail.com>
2024-01-03 15:17:35 +0100

    Remove unused code and tests for payment processing

---
+++ snippets/good_naming_is_the_best_documentation.py
@@ -0,0 +1,15 @@
+# Using non-descriptive names
+import math
+
+r = 5
+a = math.pi * r ** 2
+
+print("The area of the circle is:", a)
+
+# Using descriptive names
+import math
+
+circle_radius = 5
+circle_area = math.pi * circle_radius ** 2
+
+print("The area of the circle is:", circle_area)
